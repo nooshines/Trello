e07457949e5e4fb288fa38cd67a89f29d048a2e1
nooshines <nooshin_es26413@yahoo.com>
2020-10-06 22:29:09 +1100

    changed port

--- server/src/index.js
+++ server/src/index.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 require("./mongo");
 
 //variables
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 //setup express app
 const app = express();
